f16b6d0f1994c763323203e47a145c6b2cab35e5
surjya <surjyamaniduan@gmail.com>
2021-10-26 18:01:00 +0530

    Added: This UC Used paramitarized test

--- src/test/java/lambdaexptest/UserValidationTest.java
+++ src/test/java/lambdaexptest/UserValidationTest.java
@@ -1,5 +1,42 @@
 package lambdaexptest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import lambdaExpr.UserValidation;
 
 public class UserValidationTest {
+    @Test
+    public void firstName_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = validation.firstName("Manish");
+        Assert.assertTrue(actualResult);
+    }
 
+    @Test
+    public void lastName_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = validation.lastName("Kumar");
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void email_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = validation.email("mkaubr007@gmail.com");
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void phoneNumber_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = validation.phoneNumber("+918002695700");
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void password_Validation() {
+        UserValidation validation = new UserValidation();
+        boolean actualResult = validation.password("qw1rt@Ms");
+        Assert.assertTrue(actualResult);
+    }
 }
