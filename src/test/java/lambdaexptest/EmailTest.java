0ea21a09d0a067ef07a26c2726c0d5077e78caf9
Surjyamani Duan <88813830+surjyamanidun@users.noreply.github.com>
2021-10-29 15:27:45 +0530

    Update EmailTest.java

--- src/test/java/lambdaexptest/EmailTest.java
+++ src/test/java/lambdaexptest/EmailTest.java
@@ -1,5 +1,56 @@
 package lambdaexptest;
 
-public class EmailTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailTest {
+    private String email;
+    private boolean expectedResult;
+
+    public EmailTest(String email, boolean expectedResult) {
+        this.email = email;
+        this.expectedResult = expectedResult;
+    }
+
+    @Parameterized.Parameters
+    public static Collection emailIdsWithExceptedResult() {
+        return Arrays.asList(new Object[][]{
+                {"abc@yahoo.com", true},
+                {"abc-100@yahoo.com", true},
+                {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true},
+                {"abc-100@abc.net", true},
+                {"abc.100@abc.com.au", true},
+                {"abc@1.com", true},
+                {"abc@gmail.com.com", true},
+                {"abc+100@gmail.com", true},
+                {"abc", false},
+                {"abc@.com.my", false},
+                {"abc123@gmail.a", false},
+                {"abc123@.com", false},
+                {"abc123@.com.com", false},
+                {".abc@abc.com", false},
+                {"abc()*@gmail.com", false},
+                {"abc@%*.com", false},
+                {"abc..2002@gmail.com", false},
+                {"abc.@gmail.com", false},
+                {"abc@abc@gmail.com", false},
+                {"abc@gmail.com.1a", false},
+                {"abc@gmail.com.aa.au", false}
+        });
+    }
+
+    @Test
+    public void givenEmailIds_WithExpectedResult_ShouldPassAllTheTestCase() {
+        EmailValidation validation = new EmailValidation();
+        boolean actualResult = validation.email(this.email);
+        Assert.assertEquals(this.expectedResult, actualResult);
+    }
 }
