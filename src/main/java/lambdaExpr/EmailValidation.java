a6b7feb6fe0ff3ebbc4281403fc597abae9ff4b4
Surjyamani Duan <88813830+surjyamanidun@users.noreply.github.com>
2021-10-29 15:40:53 +0530

    Update EmailValidation.java

--- src/main/java/lambdaExpr/EmailValidation.java
+++ src/main/java/lambdaExpr/EmailValidation.java
@@ -1,5 +1,12 @@
 package lambdaExpr;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailValidation {
-
+    public static boolean email(String email) {
+        String regex = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z0-9]+.(com|net)(.[a-z]{2,3}){0,1}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
 }
