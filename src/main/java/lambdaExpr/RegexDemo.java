1d8f6609cb61097b2f6fb049cd2e032b6e90d34e
Surjyamani <88813830+surjyamanidun@users.noreply.github.com>
2021-11-01 19:22:58 +0530

    Update RegexDemo.java

--- src/main/java/lambdaExpr/RegexDemo.java
+++ src/main/java/lambdaExpr/RegexDemo.java
@@ -1,5 +1,83 @@
 package lambdaExpr;
 
-public class RegexDemo {
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class RegexDemo {
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        RegexDemo regexDemo = new RegexDemo();
+        System.out.println("Enter first name");
+        regexDemo.firstName();
+        System.out.println("Enter last name");
+        regexDemo.lastName();
+        System.out.println("Enter email");
+        regexDemo.email();
+        System.out.println("Enter mobile number");
+        regexDemo.phoneNumber();
+        System.out.println("Enter password");
+        regexDemo.password();
+    }
+
+    public void firstName() {
+        String input = sc.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+    public void lastName() {
+        String input = sc.next();
+        String regex = "^[a-zA-Z]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+    public void email() {
+        String input = sc.next();
+        String regex = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z]+.[a-z]{2,3}+.([a-z]{2,3})$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+    public void phoneNumber() {
+        String input = sc.next();
+        String regex = "(?:(?:\\+|0{0,2})91(\\s*[\\-]\\s*)?|[0]?)?[789]\\d{9}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+    public void password() {
+        String input = sc.next();
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
 }
